dc317d6d241e3ef0465e498e1362ca47cb921d95
bugmur <hiddenmarshall01@gmail.com>
2023-10-10 18:32:43 +0300

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import CustomFilter from "./CustomFilter"
+import SearchBar from "./SearchBar"
+import Hero from "./Hero"
+import CustomButton from "./CustomButton"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import SearchManufacturer from "./SearchManufacturer"
+import CarCard from "./CarCard"
+import CarDetails from "./CarDetails"
+import ShowMore from "./ShowMore"
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+}
